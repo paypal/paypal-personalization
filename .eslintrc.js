3c34bcb7097d26258d5671be69ae55fe9cf927df
Steve Mask <smask@paypal.com>
2021-12-09 14:36:32 -0500

    declarations

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@
 
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    'extends': './node_modules/grumbler-scripts/config/.eslintrc-browser.js'
+    'extends': './node_modules/grumbler-scripts/config/.eslintrc-browser.js',
+    globals: {
+        __PAYPAL_DOMAIN__: true
+    }
 };
