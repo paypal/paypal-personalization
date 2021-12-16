802f4050ac7e9e1ee115c0fc51705a9a532e30fc
Steven Mask <mnicpt@gmail.com>
2021-12-16 10:43:12 -0500

    Tagline (#5)
    Co-authored-by: Steve Mask <smask@paypal.com>

--- src/experiments/tagline/script.js
+++ src/experiments/tagline/script.js
@@ -1,5 +1,13 @@
 /* @flow */
 
-export const script : () => string = () => {
-    return '';
+import { html } from './html';
+
+export const script = ({ personalization } : {| personalization : {| text : string, tracking : {| impression : string, click : string |} |} |}) : string => {
+    return `
+        const taglineElement = document.querySelector('.paypal-button-tagline');
+        if (taglineElement) {
+            taglineElement.innerHTML = ${ personalization.text }${ html({ personalization }) };
+            
+        }
+    `;
 };
