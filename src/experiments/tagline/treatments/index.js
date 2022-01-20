471fa64ee6d9c3cc7139dfa38e731950e33b7cf8
Steve Mask <smask@paypal.com>
2022-01-20 14:23:43 -0500

    Refactor

--- src/experiments/tagline/treatments/index.js
+++ src/experiments/tagline/treatments/index.js
@@ -1,5 +1,10 @@
 /* @flow */
+/* eslint-disable import/no-namespace */
 
-export * from './tag0';
-export * from './tag1';
-export * from './tag2';
+import * as tag0 from './tag0';
+import * as tag1 from './tag1';
+import * as tag2 from './tag2';
+
+export { tag0, tag1, tag2 };
+
+/* eslint-enable import/no-namespace */
