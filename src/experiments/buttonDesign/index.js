79c718f5c4f523c839f4b46e4b94845a08bfeaf8
Steve Mask <smask@paypal.com>
2022-02-01 14:25:46 -0500

    fix flow and lint errors

--- src/experiments/buttonDesign/index.js
+++ src/experiments/buttonDesign/index.js
@@ -1,7 +1,5 @@
 /* @flow */
 
-import { treatments } from '../tagline';
-
 export * from './html';
 export * from './style';
 export * from './script';
--- src/experiments/buttonDesign/style.js
+++ src/experiments/buttonDesign/style.js
@@ -1,6 +1,7 @@
 /* @flow */
+
 import { style as getStyle } from './treatments/divideLogoAnimation';
 
-export const style = ({ personalization } : {| personalization : {| text : string, tracking : {| impression : string, click : string |} |} |}) : string => {
+export const style = () : string => {
     return getStyle();
 };
