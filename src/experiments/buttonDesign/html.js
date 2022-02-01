13439bc3951ca00992bfa09ba4d8411c2f38d12b
Steven Mask <mnicpt@gmail.com>
2022-02-01 14:28:50 -0500

    Fix Flow and Lint errors (#13)
    * fix flow and lint errors
    
    * fix flow and lint errors
    
    * fix flow and lint errors
    
    Co-authored-by: Steve Mask <smask@paypal.com>

--- src/experiments/buttonDesign/html.js
+++ src/experiments/buttonDesign/html.js
@@ -1,11 +1,5 @@
 /* @flow */
 
-export const html = ({ personalization } : {| personalization : {| text : string, tracking : {| impression : string, click : string |} |} |}) : string => {
-    if (personalization?.tracking?.impression) {
-        return `
-            <img class='tracking-beacon' src='${ personalization?.tracking?.impression }' />
-        `;
-    } else {
-        return '';
-    }
+export const html = () : string => {
+    return '';
 };
--- src/experiments/buttonDesign/script.js
+++ src/experiments/buttonDesign/script.js
@@ -1,8 +1,7 @@
 /* @flow */
 
-import { html } from './html';
 import { script as getScript } from './treatments/divideLogoAnimation';
 
-export const script = ({ personalization } : {| personalization : {| text : string, tracking : {| impression : string, click : string |} |} |}) : string => {
+export const script = () : string => {
     return getScript();
 };
