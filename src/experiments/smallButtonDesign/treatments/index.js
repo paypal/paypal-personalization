60a2b42b7c7446435b80ac9211c2b29df3970e45
Sage <sono@paypal.com>
2022-02-10 09:54:08 -0500

    add alternate logo animation and hover animation

--- src/experiments/smallButtonDesign/treatments/index.js
+++ src/experiments/smallButtonDesign/treatments/index.js
@@ -5,12 +5,16 @@ import * as revealMessageTabAnimation from './revealMessageTabAnimation';
 import * as fadeMessageAnimation from './fadeMessageAnimation';
 import * as stackLogoMessageAnimation from './stackLogoMessageAnimation';
 import * as halfLogoMessageAnimation from './halfLogoMessageAnimation';
+import * as alternateLogoMessageAnimation from './alternateLogoMessageAnimation';
+import * as hoverAlternateMessageAnimation from './hoverAlternateMessageAnimation';
 
 export {
     revealMessageTabAnimation,
     fadeMessageAnimation,
     stackLogoMessageAnimation,
-    halfLogoMessageAnimation
+    halfLogoMessageAnimation,
+    alternateLogoMessageAnimation,
+    hoverAlternateMessageAnimation
 };
 
 /* eslint-enable import/no-namespace */
