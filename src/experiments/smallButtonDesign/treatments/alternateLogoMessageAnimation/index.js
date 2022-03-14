37ab37a64aca3ffde347d449764d99b3eb78fbef
ssono <ssono4013@gmail.com>
2022-03-14 07:25:24 -0700

    Alternate logo animation (#18)
    * make button designs functional
    
    * add inline log message design
    
    * add constants
    
    * revealMessageTab animation and match existing designs to style
    
    * lint fix
    
    * fadeMessageAnimation
    
    * fix small lint
    
    * Add buttonSize eligibility
    
    * fix css for single line
    
    * stacked logo message animation
    
    * half-logo-half-message animation
    
    * add alternate logo animation and hover animation
    
    Co-authored-by: Sage <sono@paypal.com>
    Co-authored-by: Steven Mask <mnicpt@gmail.com>

---
+++ src/experiments/smallButtonDesign/treatments/alternateLogoMessageAnimation/index.js
@@ -0,0 +1,170 @@
+/* @flow */
+import { LOGO_CLASS, LOGO_COLOR } from '@paypal/sdk-logos';
+
+import { CLASS } from '../../../../constants';
+import type { ButtonDesignConfig, ButtonDesignProps } from '../../../../types';
+
+// Gets and Creates necessary HTML elements for the design
+function getDesignProps(config : ButtonDesignConfig) : ButtonDesignProps | null {
+    const designContainer = document.querySelector(`.${ config.PAYPAL_BUTTON }`);
+    if (!designContainer) {
+        return null;
+    }
+
+    const paypalLabelContainerElement = designContainer.querySelector(`.${ config.LABEL_CONTAINER }`) || null;
+    if (!paypalLabelContainerElement) {
+        return null;
+    }
+
+    // Compute css values
+    const buttonHeight = designContainer.offsetHeight;
+
+    // Add necessary HTML components
+
+    const personalizedLabelContainer = document.createElement('div');
+    personalizedLabelContainer.classList.add(config.PERSONALIZED_CONTAINER);
+
+    const designMessage = document.createElement('p');
+    designMessage.classList.add(config.PERSONALIZED_MESSAGE);
+    // designMessage.innerHTML = 'A safer easier way to pay';
+    designMessage.innerHTML = 'Life before Death, Strength before Weakness';
+
+    personalizedLabelContainer.appendChild(designMessage);
+    
+    paypalLabelContainerElement.appendChild(personalizedLabelContainer);
+    
+
+    return {
+        designContainer,
+        paypalLabelContainerElement,
+        buttonHeight
+    };
+}
+
+function applyDesign(designProps : ButtonDesignProps, config : ButtonDesignConfig) {
+    const {
+        designContainer,
+        paypalLabelContainerElement,
+        buttonHeight
+    } = designProps;
+
+
+    const fontColor = __STYLE__ && (__STYLE__.color === 'blue' || __STYLE__.color === 'black') ? 'white' : '#003087';
+
+    const designCss = `
+         .${ config.PAYPAL_BUTTON } img.${ config.PAYPAL_LOGO }-paypal {
+            animation: 4s fade-logo-left 1s infinite alternate;
+            position:fixed;
+            transform:translateX(-50%);
+        }
+
+        .${ config.PAYPAL_BUTTON } .${ config.PERSONALIZED_CONTAINER } {
+            position: fixed;
+            animation: 4s show-text 1s infinite alternate;
+            font-size: 4.5vw;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            width: 100%;
+            right: 0%;
+            height: ${ buttonHeight }px;
+            transform: translateY(-25%);
+            text-align: center;
+            color: ${ fontColor };
+        }
+
+        @keyframes fade-logo-left {
+            0%,33% {
+                transform: translateX(-50%);
+                opacity:1;
+            }
+            50%,100% {
+                position:fixed;
+                transform: translateX(-200%);
+                opacity:0;
+            }
+        }
+        @keyframes show-text {
+            0%, 35%{
+                opacity: 0;
+                transform: translate(75%, -25%);
+            }
+            56%, 100% {
+                opacity: 1;
+                transform: translate(0%, -25%);                
+            }
+        }
+    `;
+
+
+    if (paypalLabelContainerElement) {
+        const style = document.createElement('style');
+        paypalLabelContainerElement.appendChild(style);
+        style.appendChild(document.createTextNode(designCss));
+
+      
+        window.addEventListener('resize', () => {
+            // Remove animation if size limit broken
+            if (
+                (designContainer.offsetWidth > config.max || designContainer.offsetWidth < config.min)
+              && paypalLabelContainerElement.contains(style)
+            ) {
+                paypalLabelContainerElement.removeChild(style);
+            }
+        });
+    }
+}
+
+export const script = () : string => {
+
+    const config = `{
+        min: 150,
+        max: 300,
+        PAYPAL_LOGO:  '${ LOGO_CLASS.LOGO }',
+        LOGO_COLOR: '${ LOGO_COLOR }',
+        DOM_READY: '${ CLASS.DOM_READY }',
+        PAYPAL_BUTTON: '${ CLASS.PAYPAL_BUTTON }',
+        LABEL_CONTAINER: '${ CLASS.LABEL_CONTAINER }',
+        PERSONALIZED_CONTAINER: '${ CLASS.PERSONALIZED_CONTAINER }',
+        PERSONALIZED_MESSAGE: '${ CLASS.PERSONALIZED_MESSAGE }'
+    }`;
+
+    return `
+    (
+        function () {
+            const config = ${ config };
+            const designProps = (${ getDesignProps.toString() })(config)
+            if (designProps) {
+                (${ applyDesign.toString() })(designProps, config)
+            }
+        }
+    )()
+    
+  `;
+
+};
+
+export const style = () : string => {
+    return `
+
+        .${ CLASS.PAYPAL_BUTTON } .${ CLASS.PERSONALIZED_MESSAGE } {
+            text-align: center;
+            white-space: break-spaces;
+            padding: 5px;
+            width: 100%;
+            margin: 0px;
+            height: fit-content;
+        }
+
+        .${ CLASS.PAYPAL_BUTTON } .${ CLASS.PERSONALIZED_CONTAINER } {
+            opacity: 0; 
+            position: fixed;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+        }
+
+  `;
+};
+
+export const html = () : string => {
+    return '';
+};
